f715ea023ae6c64df6d8130289063961a346b715
ehzilehzil <ezhilehzil@outlook.com>
2025-08-19 13:44:37 +0900

    f

--- simple_browser/go.mod
+++ simple_browser/go.mod
@@ -3,9 +3,12 @@ module simple_browser
 go 1.25.0
 
 require (
-	github.com/jchv/go-webview2 v0.0.0-20250406165304-0bcfea011047 // indirect
+	github.com/jchv/go-webview2 v0.0.0-20250406165304-0bcfea011047
+	github.com/lxn/walk v0.0.0-20210112085537-c389da54e794
+)
+
+require (
 	github.com/jchv/go-winloader v0.0.0-20250406163304-c1995be93bd1 // indirect
-	github.com/lxn/walk v0.0.0-20210112085537-c389da54e794 // indirect
 	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
 	golang.org/x/sys v0.35.0 // indirect
 	gopkg.in/Knetic/govaluate.v3 v3.0.0 // indirect
--- simple_browser/main.go
+++ simple_browser/main.go
@@ -4,34 +4,35 @@ import (
 	"os"
 
 	"github.com/jchv/go-webview2/pkg/edge"
-	. "github.com/lxn/walk/declarative"
+	"github.com/lxn/walk"
 )
 
+// copilot.microsoft.com 에 질문한 내용과 github.com/pirogom/walkmgr 코드를 참고함
 func main() {
-	_main_view := initWebview()
+	mw, _ := walk.NewMainWindow()
 
-	MainWindow{
-		Title:  "심플 브라우저",
-		Layout: VBox{},
-		Children: []Widget{
-			ImageView{
-				Layout: VBox{}.
-				
-			},
-			Composite{
-				Layout: VBox{},
-				// AssignTo: &main_view,
-			},
-		},
-	}.Run()
-}
+	mw.SetTitle("심플 브라우저")
+	mw.SetSize(walk.Size{Width: 800, Height: 600})
+	mw.SetLayout(walk.NewVBoxLayout())
 
-func initWebview() *edge.Chromium {
-	// https://github.com/pirogom/walkmgr/blob/main/webview2.go 내용 참고
+	iv, _ := walk.NewImageView(mw)
+	iv.SetMode(walk.ImageViewModeStretch)
 
 	wv := edge.NewChromium()
 	wv.SetPermission(edge.CoreWebView2PermissionKindClipboardRead, edge.CoreWebView2PermissionStateAllow)
 	wv.DataPath = os.TempDir()
 
-	return wv
+	iv.SizeChanged().Attach(func() {
+		wv.NotifyParentWindowPositionChanged()
+		wv.Resize()
+	})
+
+	mw.Synchronize(func() {
+		wv.Embed(uintptr(iv.Handle()))
+		wv.Navigate("https://google.com")
+		wv.Resize()
+	})
+
+	mw.Show()
+	mw.Run()
 }
